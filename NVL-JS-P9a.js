19f67765682713c3482b76a0ac041dbe55cf9407
danipem <56585156+danipem@users.noreply.github.com>
2019-11-13 12:10:34 +0100

    practica

---
+++ NVL-JS-P9a.js
@@ -0,0 +1,11 @@
+var a = [[3,5], ["Hola","adios"], [1,"a"]]
+
+//longitud del array
+alert(a.length)
+
+//longitud de un elemento
+alert(a[1].length)
+
+//mostrar valor tercer elemento y de se 2o elemento (dentro del 3o)
+alert(a[2])
+alert(a[2][1])
